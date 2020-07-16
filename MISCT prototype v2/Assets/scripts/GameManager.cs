cc1d6fd850f997fdbad11e8a91dd588bc744e7b1
KnowingPigeon <chloecourage@outlook.com>
2020-07-16 10:04:13 -0700

    Prototype bug fix

--- MISCT prototype v2/Assets/scripts/GameManager.cs
+++ MISCT prototype v2/Assets/scripts/GameManager.cs
@@ -14,6 +14,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (partscollected == 2)
+        {
+            print("You win!");
+        }
     }
 
     public void PartCollide()
