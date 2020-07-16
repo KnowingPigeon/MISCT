fcf821fde140699845c7d3f987fa67b96199411b
KnowingPigeon <chloecourage@outlook.com>
2020-07-16 11:20:11 -0700

    Scripting-update

---
+++ MISCT prototype v2/Assets/scripts/AlienRiddleCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienRiddleCollision : MonoBehaviour
+{
+    AlienRiddleSpeech ARS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ARS = FindObjectOfType<AlienRiddleSpeech>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ARS.Interact();
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        ARS.InteractEnd();
+    }
+}
